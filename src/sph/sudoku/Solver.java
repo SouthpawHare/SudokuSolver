ae4b246742a416664af9a8d32ba8d90271d46366
Christopher J Czyzewski <VirtualAdept01@hotmail.com>
2019-02-05 03:08:31 -0500

    Finalized recursive algorithm using a breadth-first/depth-first hybrid approach

--- src/sph/sudoku/Solver.java
+++ src/sph/sudoku/Solver.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public class Solver {
 	
+	private static final int DEPTH_LIMIT_MAX = 10;
+
+	//Main method for running the Solver.  Accepts one parameter: the input file.
 	public static void main (String [] args) {
+		
+		//Require the input file parameter
 		if (args == null || args.length != 1) {
 			System.out.println("Please provide an input filename");
 			System.exit(1);
@@ -18,11 +23,13 @@ public class Solver {
 		
 		System.out.println(String.format("Running on file %s", fileInput.toString()));
 		
+		//Make sure input file is valid
 		if (!fileInput.exists() || !fileInput.isFile()) {
 			System.out.println("Invalid file provided");
 			System.exit(1);
 		}
 		
+		//Create the initial puzzle state from the input file.
 		Puzzle original = null;
 		try {
 			original = Puzzle.createPuzzleFromInput(fileInput);
@@ -38,6 +45,7 @@ public class Solver {
 		System.out.println("\nOriginal:");
 		System.out.println(original.toString());
 		
+		//Attempt to solve the puzzle and acquire a solution
 		Puzzle solution = Solver.solve(original);
 		
 		if (solution == null) {
@@ -55,6 +63,7 @@ public class Solver {
 			System.out.println("The puzzle was not completed");
 		}
 
+		//Output the solution to the output file
 		File fileOutput = new File(fileInput.getName() + ".sln.txt");
 		BufferedWriter writer = null;
 		try {
@@ -66,6 +75,7 @@ public class Solver {
 			System.out.println("Could not output to output file");
 		}
 		finally {
+			//Clean up output file
 			if (writer != null) {
 				try {
 					writer.close();
@@ -77,35 +87,60 @@ public class Solver {
 	}
 	
 	public static Puzzle solve(Puzzle puzzle) {
-		return solveWorker(puzzle.copy());
+		Puzzle lastResult = puzzle;
+		
+		//Attempt to solve the puzzle with a limit to the recursive depth.  Each time
+		//it fails at the current recursive depth limit, increase the limit by 1 and
+		//start again from the beginning.  This allows for a hybrid style of depth-
+		//first and breadth-first methods.
+		for (int depthLimit = 0; depthLimit <= DEPTH_LIMIT_MAX; depthLimit++) {
+			System.out.println(String.format("Current Guess Depth Limit: %d", depthLimit));
+			lastResult = solveWorker(puzzle.copy(), 0, depthLimit);
+			if (lastResult.isFilled()) {
+				return lastResult;
+			}
+		}
+		return lastResult;
 	}
 	
-	private static Puzzle solveWorker(Puzzle puzzle) {
+	private static Puzzle solveWorker(Puzzle puzzle, int currGuessDepth, int guessDepthLimit) {
+		
+		//While there continue to be spaces with only one possible value,
+		//continue to fill them in.  Stop if no progress was made in the
+		//last filling attempt or if the puzzle is finished.
 		while (!puzzle.isFilled() && puzzle.getStatus().isContinueWithOverall()) {
 			puzzle.solveNext();
 		}
 		
+		//If the puzzle is not solved and there are only squares left that have more than one
+		//possible answer, we must make a recursive guess.
 		if (!puzzle.isFilled() && puzzle.getStatus() != SolveStatus.Impossible) {
-			List<GuessData> guesses = puzzle.getGuessesInPriorityOrder();
-			for (GuessData guess : guesses) {
-				for (Integer possibleVal : guess.getPossibleValues()) {
+			
+			//We will only go to a recursive depth within our current depth limit, in order
+			//to provide a breadth-first algorithm.
+			if (currGuessDepth < guessDepthLimit) {
+				
+				//Squares to guess are prioritized by number of possibilities.  Squares with
+				//the fewest possibilities with be tried first.
+				List<GuessData> guesses = puzzle.getGuessesInPriorityOrder();
+				
+				//For every square with one than one possibility:
+				for (GuessData guess : guesses) {
+					//For each possibility:
+					for (Integer possibleVal : guess.getPossibleValues()) {
+						
+						Puzzle puzzleCopy = puzzle.copy();
+						puzzleCopy.getSquare(guess.getRow(), guess.getCol()).setVal(possibleVal);
+						puzzleCopy = solveWorker(puzzleCopy, currGuessDepth+1, guessDepthLimit);
 
-					//							System.out.println(String.format("Making a guess at row %d, col %d: Possible values = %s, guessing %d",
-					//									row, col, possibleValues.toString(), possibleVal));
-
-					Puzzle puzzleCopy = puzzle.copy();
-					puzzleCopy.getSquare(guess.getRow(), guess.getCol()).setVal(possibleVal);
-					puzzleCopy = solveWorker(puzzleCopy);
-
-					if (puzzleCopy.isFilled()) {
-						return puzzleCopy;
+						if (puzzleCopy.isFilled()) {
+							return puzzleCopy;
+						}
 					}
-
-					//							System.out.println("Guess yielded no results");
 				}
 			}
 		}
-		
+
 		return puzzle;
 	}
 
