7a32c90ceb064e4db36632bba4ff65147bb523a6
Christopher J Czyzewski <VirtualAdept01@hotmail.com>
2019-02-05 05:10:16 -0500

    Added additional comments and documentation, plus tests

---
+++ test/sph/sudoku/PuzzleTest.java
@@ -0,0 +1,71 @@
+package sph.sudoku;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class PuzzleTest {
+	
+	@Test
+	void testCreateEmptyPuzzle() {
+		Puzzle puzzle = Puzzle.createEmptyPuzzle();
+		assertTrue(puzzle.isValidSize());
+	}
+	
+	@Test
+	void testGetSquaresInBox() {
+		Puzzle puzzle = Puzzle.createEmptyPuzzle();
+		
+		List<Square> relatedSquares = puzzle.getSquaresInBox(8, 0);
+		
+		boolean[][] relationships = new boolean[][] {
+			{false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false},
+			{false, false, false, false, false, false, false, false, false},
+			{true , true , true , false, false, false, false, false, false},
+			{true , true , true , false, false, false, false, false, false},
+			{true , true , true , false, false, false, false, false, false}
+		};
+		
+		checkAllRelationships(puzzle, relatedSquares, relationships);
+	}
+
+	@Test
+	void testGetRelatedSquares() {
+		Puzzle puzzle = Puzzle.createEmptyPuzzle();
+		
+		List<Square> relatedSquares = puzzle.getRelatedSquares(4, 6);
+		
+		boolean[][] relationships = new boolean[][] {
+			{false, false, false, false, false, false, true , false, false},
+			{false, false, false, false, false, false, true , false, false},
+			{false, false, false, false, false, false, true , false, false},
+			{false, false, false, false, false, false, true , true , true },
+			{true , true , true , true , true , true , true , true , true },
+			{false, false, false, false, false, false, true , true , true },
+			{false, false, false, false, false, false, true , false, false},
+			{false, false, false, false, false, false, true , false, false},
+			{false, false, false, false, false, false, true , false, false},
+		};
+		
+		checkAllRelationships(puzzle, relatedSquares, relationships);
+		
+	}
+
+	private void checkAllRelationships(Puzzle puzzle, List<Square> relatedSquares, boolean[][] relationships) {
+		for (int row = 0; row < relationships.length; row++) {
+			for (int col = 0; col < relationships[row].length; col++) {
+				Square square = puzzle.getSquare(row, col);
+				assertEquals(relationships[row][col], relatedSquares.contains(square));
+			}
+		}
+	}
+	
+	
+
+}
